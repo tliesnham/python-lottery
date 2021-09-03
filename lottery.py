74f03a3eb86ea58f39ff5f76e2fb751518bf566a
Taylor Liesnham <taylorliesnham@Taylors-MacBook-Air-10.local>
2021-09-03 15:05:15 +0100

    first commit

---
+++ lottery.py
@@ -0,0 +1,22 @@
+from random import shuffle
+
+def draw():
+    balls = [x for x in range(1,60)]
+    shuffle(balls)
+
+    numbers = balls[:3]
+    numbers.sort()
+
+    return numbers
+
+def checkResults(ticket, draw):
+    return draw == ticket
+
+# generate our ticket
+ticket = draw()
+
+i = 0
+while checkResults(ticket, draw()) is not True:
+    i += 1
+
+print(f"You won after {i} draws.")
